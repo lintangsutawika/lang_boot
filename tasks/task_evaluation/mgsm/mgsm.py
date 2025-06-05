da73a8e637dc08b85c257e5c6976d85a2d3d2cc3
lintangsutawika <lsutawik@andrew.cmu.edu>
2025-06-05 19:38:58 -0400

    fix mgsm

--- tasks/task_evaluation/mgsm/mgsm.py
+++ tasks/task_evaluation/mgsm/mgsm.py
@@ -12,6 +12,7 @@ from yeval.response import (
     )
 
 from yeval.metrics import math_eval
+from yeval.response.math_responses import get_boxed_answer
 
 path = os.path.dirname(__file__)
 
@@ -21,21 +22,32 @@ class MGSMTask(YevalTask):
     output_text=lambda x: x["answer_number"]
     test_split="test"
     evaluation={"accuracy": math_eval}
+    postprocessor=get_boxed_answer
 
 @register_task("mgsm_eng")
-class MGSM_ENTask(MGSMTask):
+class MGSM_EN_Task(MGSMTask):
     data_name="en"
 
 @register_task("mgsm_zho")
-class MGSM_ENTask(MGSMTask):
+class MGSM_ZH_Task(MGSMTask):
     data_name="zh"
 
 @register_task("mgsm_jpn")
-class MGSM_ENTask(MGSMTask):
+class MGSM_JA_Task(MGSMTask):
     data_name="ja"
 
+@register_task("mgsm_fra")
+class MGSM_FR_Task(MGSMTask):
+    data_name="fr"
+
 @register_task("mgsm_ind")
-class MGSM_IDTask(MGSMTask):
+class MGSM_ID_Task(MGSMTask):
     data_path="json"
     data_name=None
     data_kwargs={"data_files": {"test": os.path.join(path, "mgsm_id.jsonl")}}
+
+@register_task("mgsm_translate")
+class MGSMTranslateTask(YevalTask):
+    system_message="You are a helpful assistant that can translate from English to Indonesian. Start your translation after ####"
+    user_message=lambda x: "English Query:\n\n"+f"```\n{x}\n```"+"Indonesian Translation:\n\n"
+    sampling_args={"n": 4}
