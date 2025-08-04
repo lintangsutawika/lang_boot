3168824a9a806e97670d90c03bcf76b9ca8ef29d
lintang <lintang@sutawika.com>
2025-08-04 13:50:48 -0400

    return dict

--- lang_boot/reward_functions/reward_fn.py
+++ lang_boot/reward_functions/reward_fn.py
@@ -10,6 +10,8 @@ def compute_score(data_source, solution_str, ground_truth, extra_info=None, use_
 
     try:
         gold = get_boxed_answer(ground_truth)
+        if gold == "None":
+            gold = ground_truth
     except:
         gold = ground_truth
 
@@ -18,16 +20,26 @@ def compute_score(data_source, solution_str, ground_truth, extra_info=None, use_
     except:
         ans = solution_str
 
-    # ans_score = math_eval_with_postprocessing(solution_str, gold)
-    ans_score = math_eval(ans, gold)
-    _, lang_score = get_lang_score(solution_str, lang=extra_info["lang"])
+    ans_score = 0.0
+    if extra_info["task"] == "mmlu-global":
+        gold_letter, gold_answer = gold.split(":::")
+        if ans.lower() == gold_letter.lower():
+            ans_score = 1.0
+        elif ans.lower() == gold_answer.lower():
+            ans_score = 1.0
+        elif ans.lower()[:1] == gold_letter.lower():
+            ans_score = 1.0
+    else:
+        ans_score = math_eval(ans, gold)
 
-    if use_lang:
+    lang = extra_info["lang"]
+    if use_lang and (lang != "en"):
+        _, lang_score = get_lang_score(solution_str, lang=lang)
         reward = ans_score * lang_score
     else:
         reward = ans_score
 
-    if use_penalty:
+    if use_penalty and (lang != "en"):
         penalty = 0
         N_gram_sizes = [2, 3, 4, 5]
         for N in N_gram_sizes:
@@ -35,4 +47,9 @@ def compute_score(data_source, solution_str, ground_truth, extra_info=None, use_
         penalty /= len(N_gram_sizes)
         reward -= penalty
 
-    return reward
+    return {
+        "score": reward,
+        "task_score": ans_score,
+        "lang_score": lang_score if use_lang else 1.0,
+        "repetition_penalty": penalty if use_penalty else 0.0,
+    }
