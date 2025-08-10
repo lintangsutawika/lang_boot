a15aa656c33e87078101bdd71488fc9a1eeefbc7
lintang <lintang@sutawika.com>
2025-08-10 18:36:26 -0400

    make reward ready for any defined task

--- lang_boot/reward_functions/reward_fn.py
+++ lang_boot/reward_functions/reward_fn.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from yeval.metrics import math_eval
@@ -6,50 +7,70 @@ from lang_boot.utils import math_eval_with_postprocessing, get_lang_score
 
 from lang_boot.reward_functions.repetition_penalty import repetition_penalty
 
+from yeval.task import TASK_LIST
+from yeval.utils import import_modules
+
+path = os.path.dirname(__file__)
+import_modules(os.path.join(path, "../../tasks/"))
+
+# Make dict for all tasks,
+eval_tasks = ["mgsm", "global_mmlu", "belebele", "mt_aime2024", "mt_math100"]
+
+eval_fn = {
+    "math500": TASK_LIST["math500"]().eval,
+    **{task: TASK_LIST[f"{task}_en"]().eval for task in eval_tasks}
+}
+
 def compute_score(data_source, solution_str, ground_truth, extra_info=None, use_lang=True, use_penalty=True):
 
-    try:
-        gold = get_boxed_answer(ground_truth)
-        if gold == "None":
+    if extra_info["task"] == "train_dataset":
+        try:
+            gold = get_boxed_answer(ground_truth)
+            if gold == "None":
+                gold = ground_truth
+        except:
             gold = ground_truth
-    except:
-        gold = ground_truth
 
-    try:
-        ans = get_boxed_answer(solution_str)
-    except:
-        ans = solution_str
-
-    ans_score = 0.0
-    if extra_info["task"] == "mmlu-global":
-        gold_letter, gold_answer = gold.split(":::")
-        if ans.lower() == gold_letter.lower():
-            ans_score = 1.0
-        elif ans.lower() == gold_answer.lower():
-            ans_score = 1.0
-        elif ans.lower()[:1] == gold_letter.lower():
-            ans_score = 1.0
-    else:
+        try:
+            ans = get_boxed_answer(solution_str)
+        except:
+            ans = solution_str
+        
         ans_score = math_eval(ans, gold)
+    else:
+        task_eval = extra_info["task"].split("/")[0]
+        ans_score = eval_fn[task_eval](solution_str, ground_truth)["accuracy"]
 
     lang = extra_info["lang"]
+    _, lang_score = get_lang_score(solution_str, lang=lang)
     if use_lang and (lang != "en"):
-        _, lang_score = get_lang_score(solution_str, lang=lang)
         reward = ans_score * lang_score
     else:
         reward = ans_score
 
+    penalty = 0
+    N_gram_sizes = [2, 3, 4, 5]
+    for N in N_gram_sizes:
+        penalty += repetition_penalty(solution_str, window_size=N)
+    penalty /= len(N_gram_sizes)
+
     if use_penalty and (lang != "en"):
-        penalty = 0
-        N_gram_sizes = [2, 3, 4, 5]
-        for N in N_gram_sizes:
-            penalty += repetition_penalty(solution_str, window_size=N)
-        penalty /= len(N_gram_sizes)
         reward -= penalty
 
     return {
+        "data_source": extra_info["task"],
         "score": reward,
         "task_score": ans_score,
-        "lang_score": lang_score if use_lang else 1.0,
-        "repetition_penalty": penalty if use_penalty else 0.0,
+        "lang_score": lang_score,
+        "repetition_penalty": penalty,
+        "use_lang": use_lang,
+        "use_penalty": use_penalty,
+        "gold": ground_truth,
     }
+
+def compute_score_no_lang_no_penalty(data_source, solution_str, ground_truth, extra_info):
+    return compute_score(
+        data_source, solution_str, ground_truth, extra_info,
+        use_lang=False,
+        use_penalty=False,
+    )
